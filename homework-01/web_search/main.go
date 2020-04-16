bb0d88e9090cf988d228a9e09ae9d069ce3fd349
Vladimir <56544212+bornrussian@users.noreply.github.com>
2020-04-16 19:09:48 +0700

    Добавил распределение поиска сайтов по потокам и аргументы командной строки

--- homework-01/web_search/main.go
+++ homework-01/web_search/main.go
@@ -11,35 +11,77 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 )
+
+// Проверяем на одном конкретном сайте, есть ли там искомый контент
+func checkOneSiteHasContent (site string, content string) bool {
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Error!:", err)
+		return false
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	if strings.Contains(string(body), content) {
+		return true
+	}
+	return false
+}
 
 // Берём массив со ссылками на сайт, и проверяем, не найдется ли там content
 // Результат - массив с ссылками, где нашлось.
-
-func whichSitesHaveContent (sites []string, content string) []string {
+type channelMessage struct {
+	site string
+	found bool
+}
+func whichSitesHasContent (sites []string, needle string) []string {
 	result := []string{}
-	for _, site := range sites {
-		resp, err := http.Get(site)
-		if err != nil {
-			fmt.Println("Error!:", err)
-		} else {
-			defer resp.Body.Close()
-			body, _ := ioutil.ReadAll(resp.Body)
-			//fmt.Println("processing site: ", site, "...")
-			if strings.Contains(string(body), content) {
-				result = append(result, site)
-			}
-		}
+	mutex := &sync.Mutex{}
+
+	company := &sync.WaitGroup{}
+	for id := 0; id < len(sites); id++ {
+		company.Add(1)
+		go worker(company, mutex, sites[id], needle, &result)
 	}
+
+	company.Wait()
+
 	return result
 }
 
+func worker(wg *sync.WaitGroup, mu *sync.Mutex, site string, needle string, found *[]string) {
+	defer func () {
+		//fmt.Println("Worker закончил проверять сайт", site)
+		wg.Done()
+	} ()
+	if checkOneSiteHasContent(site, needle) {
+		//fmt.Println("Worker нашёл совпадение на сайте", site)
+		mu.Lock()
+		*found = append(*found, site)
+		mu.Unlock()
+	} else {
+		//fmt.Println("Worker не нашёл совпадение на сайте", site)
+	}
+}
+
 func main () {
-	sites := []string{"https://google.ru", "https://yandex.ru", "https://mail.ru/", "https://lenta.ru", "https://github.com"}
-	fmt.Println("search sites are:", sites)
-	fmt.Println("'google-analytics' found at: ",whichSitesHaveContent(sites, "google-analytics"))
+	flagUrls := flag.String("urls", "https://google.ru,https://mail.ru/,https://lenta.ru,https://github.com", "URLs separated by comma (,)")
+	flagSearch := flag.String("search", "google-analytics", "String to find to")
+	flag.Parse()
+
+	fmt.Println("Утилита для поиска строчки в содержимом веб-страниц")
+	fmt.Println("Принимаемые аргументы командной строки:")
+	fmt.Println("  --urls \"https://google.ru,https://yandex.ru,https://mail.ru/\"")
+	fmt.Println("  --search \"google-analytics\"")
+	fmt.Println("")
+
+	sites := strings.Split(*flagUrls,",")
+	fmt.Println("Ищем на сайтах:", sites)
+	fmt.Printf("Фраза '%v' найдена на сайтах: %v",*flagSearch,whichSitesHasContent(sites, *flagSearch))
 }
